be19dad51b166e1a6f8c39258b1f69f5479893c7
1motiur4 <92241810+1motiur4@users.noreply.github.com>
2022-12-03 13:00:44 -0500

    Created project

---
+++ src/components/CreateArea.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import AddIcon from "@mui/icons-material/Add";
+import Fab from "@mui/material/Fab";
+import Zoom from "@mui/material/Zoom";
+
+function CreateArea(props) {
+    
+}
