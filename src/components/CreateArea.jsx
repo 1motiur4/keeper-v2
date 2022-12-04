2864fdaf53469be1aa4a30f2afdb85834bdc07d3
1motiur4 <92241810+1motiur4@users.noreply.github.com>
2022-12-04 13:10:05 -0500

    Fully functioning

--- src/components/CreateArea.jsx
+++ src/components/CreateArea.jsx
@@ -1,8 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import AddIcon from "@mui/icons-material/Add";
 import Fab from "@mui/material/Fab";
 import Zoom from "@mui/material/Zoom";
 
 function CreateArea(props) {
-    
+    //hook for creating note
+    const [note, setNote] = useState({
+        title: "",
+        content: ""
+    });
+
+    function handleChange(event) {
+        const { name, value } = event.target;
+
+        setNote(prevNote => {
+            return {
+                ...prevNote,
+                [name]: value
+            }
+        })
+    }
+
+    function submitNote(event) {
+        props.onAdd(note);
+        setNote({
+            title: "",
+            content: ""
+        });
+        event.preventDefault();
+    }
+
+    return (
+        <div>
+            <form className="create-note">
+                <input name="title" onChange={handleChange} value={note.title} placeholder="Title" />
+                <textarea name="content" onChange={handleChange} value={note.content} placeholder="Write something cool" rows="3" />
+                <Fab onClick={submitNote}>
+                    <AddIcon />
+                </Fab>
+            </form>
+        </div>
+    )
 }
+
+export default CreateArea;
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -7,10 +7,10 @@ function Note(props) {
     }
 
     return (
-        <div>
+        <div className="note">
             <h1>{props.title}</h1>
             <p>{props.content}</p>
-            <button onDelete={handleClick}>
+            <button onClick={handleClick}>
                 <DeleteIcon />
             </button>
         </div>
